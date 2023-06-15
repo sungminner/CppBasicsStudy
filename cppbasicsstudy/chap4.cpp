ffd49dbeff746adad87e8ab8f63f7eecd2d3d783
sxj754 <sxj754@case.edu>
2023-06-15 14:19:57 +0900

    Chapter 4 string

--- cppbasicsstudy/chap4.cpp
+++ cppbasicsstudy/chap4.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include "chap4.h"
 
 namespace chap4 {
 	void chap4Study() {
 		// array basics
+		std::cout << "===== array basics =====" << std::endl;
 		int arr1[3] = { 3, 4, 5 };
 		int arr2[2];
 		// arr2 = { 1, 2 }; // wrong
@@ -20,6 +22,7 @@ namespace chap4 {
 		float arr6[10]{}; // can initialize every element with 0
 
 		// string
+		std::cout << "\n===== string =====" << std::endl;
 		char str1[8] = { 'b', 'e', 'a', 'u', 'x', ' ', 'I', '\0'}; // string has to be finished with null ('\0')
 		std::cout << str1 << std::endl;
 		char str2[] = "Hello, World!"; // no need to put \0
@@ -31,13 +34,30 @@ namespace chap4 {
 		std::cout << sizeof(str2) << std::endl;
 
 		// getting one line
+		std::cout << "\n===== getting one line =====" << std::endl;
 		char str3[20];
 		std::cin.getline(str3, 20);
 		std::cout << str3 << std::endl;
-		std::cin.get(str3, 5);
+		std::cin.get(str3, 5); // limit of the chars = 5 - 1 = 4
 		std::cin.get(); // use .get() to get next string properly
 		std::cout << str3 << std::endl;
 		std::cin.get(str3, 5).get();// or directly append .get to the previous get()
 		std::cout << str3 << std::endl;
+
+		// string class
+		std::cout << "\n===== string class =====" << std::endl;
+		std::string str4 = "panther";
+		std::cout << str4 << std::endl;
+		std::cout << str4[2] << std::endl;
+		std::string str5 = str4; // can directly substitute another string; not possible between char arrays
+		std::cout << str5 << std::endl;
+		std::cout << str4 + str5 << std::endl;
+		std::cout << str4 + " hello" << std::endl;
+		std::cout << str4.size() << std::endl; // string length of std::string class
+		std::cout << strlen(str3) << std::endl; // string length of char *
+
+		std::string str6;
+		getline(std::cin, str6); // read one whole line; syntax is different
+		std::cout << str6 << std::endl;
 	}
 }
